fc581ef7d10ff4071e5f2084030833572cc86679
aidatarrah <aida.tarrah@gmail.com>
2021-06-08 23:53:53 -0700

    1

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+
+
+
+jQuery(function() {
+
+	$("body").hover(function(){
+						 $("#overlay").animate({
+								 width: "toggle"
+						 });
+				 });
+});
+
+
+jQuery(function() {
+
+	$("body").hover(function(){
+						 $("h1").animate({
+								 width: "toggle"
+						 });
+				 });
+});
+
+
+  $(document).ready(function(){
+    $("body").hover(function(){
+      $(this).css("background-image", "url(img/landing_page_main_hover.jpg)");
+      /*document.getElementById("yellowOverlay").style.display = "block";*/
+    }, function(){
+      $(this).css("background-image", "url(img/landing_page_main.jpg)");
+			/*document.getElementById("yellowOverlay").style.display = "none";*/
+    });
+
+
+		$("body").hover(function(){
+							 $("#yellowOverlay").animate({
+									 width: "toggle"
+							 });
+					 });
+
+
+  });
